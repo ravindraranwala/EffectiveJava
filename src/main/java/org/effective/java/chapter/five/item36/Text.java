2b882b16ed1f133cbcdc484ae22a2048d335f95e
Ranwala <URANWRA@PEROOT.COM>
2018-08-23 10:46:48 +0530

    Item-36 was completed.

---
+++ src/main/java/org/effective/java/chapter/five/item36/Text.java
@@ -0,0 +1,13 @@
+package org.effective.java.chapter.five.item36;
+
+import java.util.Set;
+
+public class Text {
+	public enum Style {
+		BOLD, ITALIC, UNDERLINE, STRIKETHROUGH
+	};
+
+	public void applyStyles(Set<Style> styles) {
+		System.out.println(styles);
+	}
+}
