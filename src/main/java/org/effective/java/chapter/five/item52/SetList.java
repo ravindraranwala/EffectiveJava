d96e07912a8c1481a5e31fb166f09ea340025afd
Ranwala <URANWRA@PEROOT.COM>
2018-10-02 11:42:01 +0530

    Item 52 was completed.

---
+++ src/main/java/org/effective/java/chapter/five/item52/SetList.java
@@ -0,0 +1,22 @@
+package org.effective.java.chapter.five.item52;
+import java.util.*;
+
+// What does this program print? (Page 241)
+public class SetList {
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = -3; i < 3; i++) {
+            set.add(i);
+            list.add(i);
+        }
+        for (int i = 0; i < 3; i++) {
+            set.remove(i);
+            list.remove(i);
+            // Corrected version
+            // list.remove(Integer.valueOf(i));
+        }
+        System.out.println(set + " " + list);
+    }
+}
