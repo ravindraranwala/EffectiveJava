18c4d009accd08a784094059d0201115508537c3
Ranwala <URANWRA@PEROOT.COM>
2019-01-04 15:12:49 +0530

    item88 was completed.

---
+++ src/main/java/org/effective/java/chapter/twelve/item88/Period.java
@@ -0,0 +1,55 @@
+package org.effective.java.chapter.twelve.item88;
+
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.Date;
+
+//Immutable class that uses defensive copying
+public final class Period implements Serializable {
+	private static final long serialVersionUID = -8360243618692079866L;
+	private Date start;
+	private Date end;
+
+	/**
+	 * @param start
+	 *            the beginning of the period
+	 * @param end
+	 *            the end of the period; must not precede start
+	 * @throws IllegalArgumentException
+	 *             if start is after end
+	 * @throws NullPointerException
+	 *             if start or end is null
+	 */
+	public Period(Date start, Date end) {
+		this.start = new Date(start.getTime());
+		this.end = new Date(end.getTime());
+		if (this.start.compareTo(this.end) > 0)
+			throw new IllegalArgumentException(start + " after " + end);
+	}
+
+	public Date start() {
+		return new Date(start.getTime());
+	}
+
+	public Date end() {
+		return new Date(end.getTime());
+	}
+
+	public String toString() {
+		return start + " - " + end;
+	}
+
+	// readObject method with defensive copying and validity checking
+	private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+		s.defaultReadObject();
+		// Defensively copy our mutable components
+		start = new Date(start.getTime());
+		end = new Date(end.getTime());
+		// Check that our invariants are satisfied
+		if (start.compareTo(end) > 0)
+			throw new InvalidObjectException(start + " after " + end);
+	}
+	// Remainder omitted
+}
